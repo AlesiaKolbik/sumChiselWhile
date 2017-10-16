49de2e2bbf248aee89410eda02913b97823719da
alesya.kolbik <moyaalesia@mail.ru>
2017-10-16 10:46:02 +0300

    Ввычислить с помощью оператоора while и распечатать сумму чисел от 1 до 200.

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 200;
+        int counter = 1;
+        while (sum <= 200) {
+            sum = sum + counter;
+            counter++;
+            System.out.println("Сумма чисел от 1 до 200 равна " + sum );
+            }
+
+    }
+}
